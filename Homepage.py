66442a1d57f88e051cf27786868bc471f016b556
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-09-26 22:39:11 +0000

    commit

--- Homepage.py
+++ Homepage.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
-st.title('Welcome')
+# st.title('Welcome')
+
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/virgoaugustine/Ghana-GeoJSON-data/master/ghana_regions.json') as response:
+    counties = json.load(response)
+    
+for k,v in counties.items():
+    st.write(v)    
+
+import pandas as pd
+
+# df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+#                    dtype={"fips": str})
+df = pd.read_csv('data.csv')
+import plotly.express as px
+
+fig = px.choropleth(df, geojson=counties, locations='Region', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="africa",
+                           featureidkey="properties.region",
+                           labels={'unemp':'Unemployment rate'}
+                          )
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+st.plotly_chart(fig)
